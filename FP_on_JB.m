4c2da1ae29f09497972ffc0ddd35c3cf130b612e
Umeh Chukwuebuka Emmanuel <81016410+Bukson01@users.noreply.github.com>
2024-05-15 01:50:40 +0100

    updated
    Took some constants into consideration to aid solving

--- FP_on_JB.m
+++ FP_on_JB.m
@@ -2,4 +2,12 @@ close all;clear ;clc
 %DIMENSIONLESS POWER COMPUTATION%
 
 syms  g
-r  = -5 : 0.001 : 5;  % ratio of the lengths
+r  = -5 : 0.001 : 5;  % ratio of the lengths (L_3/L_1)
+
+%% (Free-Particle = FP)
+% constants
+a = 3*(4^(1/3)-1);
+b = 1/4;
+c = 1/4^(1/3);
+p = a*b;
+q = a*c;
